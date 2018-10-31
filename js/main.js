6c97524416534b361ec1c1d7343dbbc6e09c6d61
lab14 <lab14@lup.edu>
2018-10-31 20:12:48 -0300

    Actualizado index para que muestre la pantalla chat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
+// TAREA: Lo repetido pasarlo a una sola funcion y llamarla desde el index.html
 $('.toggle').click(function(){
-    $('.formulario').animate({
+    $('#login_form').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+    $('#signup_form').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+});
+$('#login').submit(function(){
+    $('#login_form').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+    $('#chat_room').animate({
+        height: "toggle",
+        'padding-top': 'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+    }, "slow");
+    $('.toggle').animate({
         height: "toggle",
         'padding-top': 'toggle',
         'padding-bottom': 'toggle',
