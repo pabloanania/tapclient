53428454fd56691bffa59df6523f6dd4e07e93af
lab14 <lab14@lup.edu>
2018-10-31 20:35:48 -0300

    Añadida funcion unica para manejo de toggle

--- js/main.js
+++ js/main.js
@@ -1,35 +1,8 @@
-// TAREA: Lo repetido pasarlo a una sola funcion y llamarla desde el index.html
-$('.toggle').click(function(){
-    $('#login_form').animate({
+function screenToggler(selector){
+    $(selector).animate({
         height: "toggle",
         'padding-top': 'toggle',
         'padding-bottom': 'toggle',
         opacity: 'toggle'
     }, "slow");
-    $('#signup_form').animate({
-        height: "toggle",
-        'padding-top': 'toggle',
-        'padding-bottom': 'toggle',
-        opacity: 'toggle'
-    }, "slow");
-});
-$('#login').submit(function(){
-    $('#login_form').animate({
-        height: "toggle",
-        'padding-top': 'toggle',
-        'padding-bottom': 'toggle',
-        opacity: 'toggle'
-    }, "slow");
-    $('#chat_room').animate({
-        height: "toggle",
-        'padding-top': 'toggle',
-        'padding-bottom': 'toggle',
-        opacity: 'toggle'
-    }, "slow");
-    $('.toggle').animate({
-        height: "toggle",
-        'padding-top': 'toggle',
-        'padding-bottom': 'toggle',
-        opacity: 'toggle'
-    }, "slow");
-});
+}
